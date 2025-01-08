9bc108e48b9da2f103d31cf8522ccd20bbc757c1
luigifg <luigi.gerber@hotmail.com>
2025-01-08 16:01:13 -0300

    git fix vite

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,12 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-
+  server: {
+    proxy: {
+      "/me": "https://localhost:3309", // URL do backend
+      "/userGroup": "https://localhost:3309", // URL do backend
+    },
+  },
   plugins: [react()],
   optimizeDeps: {
     include: ["qrcode.react"],
