a57f433ec1659aa80f5ac3a16c9d0f86a63aded4
luigifg <luigi.gerber@hotmail.com>
2025-01-10 08:52:36 -0300

    git add proxy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,12 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/me": "https://feday-api.onrender.com", // URL do backend
+      "/userGroup": "https://feday-api.onrender.com", // URL do backend
+    },
+  },
   plugins: [react()],
   optimizeDeps: {
     include: ["qrcode.react"],
