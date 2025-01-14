b4baa211466593c2536ff47200b5f6cf20844c41
luigifg <luigi.gerber@hotmail.com>
2025-01-14 18:15:08 -0300

    git fix axios

--- src/Axios.jsx
+++ src/Axios.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://api.futuredaybrasil.com.br',
+  baseURL: import.meta.env.VITE_API_BASE_URL, // Lê a URL do arquivo .env
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
