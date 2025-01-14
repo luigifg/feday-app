761f2bb63ae826859d7a5e3f2a4d13bf0b568c00
luigifg <luigi.gerber@hotmail.com>
2025-01-14 17:41:01 -0300

    git fix .env and axios

--- vite.config.js
+++ vite.config.js
@@ -3,22 +3,16 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      "/me": "https://api.futuredaybrasil.com.br", // URL do backend
-      "/userGroup": "https://api.futuredaybrasil.com.br", // URL do backend
-    },
-  },
   build: {
-    assetsInlineLimit: 0, // Desativa a conversão para base64
+    assetsInlineLimit: 0, // Desativa a conversão para base64 para arquivos grandes
     rollupOptions: {
       output: {
-        assetFileNames: 'assets/[name]-[hash][extname]'
-      }
-    }
+        assetFileNames: "assets/[name]-[hash][extname]", // Organiza os arquivos de saída
+      },
+    },
   },
   plugins: [react()],
   optimizeDeps: {
-    include: ["qrcode.react"],
+    include: ["qrcode.react"], // Dependências que devem ser pré-otimizadas
   },
 });
