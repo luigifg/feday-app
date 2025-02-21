2e914fa2567d9487036ae3ebd1bc89933a27a0db
luigifg <luigi.gerber@hotmail.com>
2025-02-21 10:54:47 -0300

    git add second image

--- src/Views/Home/Hero.jsx
+++ src/Views/Home/Hero.jsx
@@ -2,9 +2,12 @@ import React, { useState, useEffect, useRef } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { curve } from "../../assets";
 import bg1 from "../../assets/logos/bg1.svg";
+
 import bg2 from "../../assets/logos/bg2.svg";
 import fe2025 from "../../assets/logos/fedayDateR2.png";
+import fe2025M from "../../assets/logos/feday2025M.png";
 import keynote1 from "../../assets/speakers/barrera4.png";
+import keynote1M from "../../assets/speakers/barreraM.png";
 import Button from "../../Components/design/Button";
 import Section from "../../Components/Section";
 import CompanyLogos from "./CompanyLogos";
@@ -53,6 +56,7 @@ const Hero = () => {
     {
       id: 1,
       logo: fe2025,
+      mobileImage: fe2025M,
       welcome: "Bem-vindo ao evento",
       title: "Future Day 2025",
       description:
@@ -62,12 +66,12 @@ const Hero = () => {
     {
       id: 2,
       logo: keynote1,
+      mobileImage: keynote1M,
       welcome: "Conheça nosso",
       title: "Palestrante Especial",
       description:
         "Conheça Conheça Conheça Conheça Conheça Conheça Conheça nossos palestrantes internacionais que trarão as últimas tendências e inovações em tecnologia e eletrônica",
       buttonText: "Conheça Fernando Barrera",
-
     },
   ];
 
@@ -146,7 +150,7 @@ const Hero = () => {
             className="relative z-10 w-full"
           >
             {/* Slides Content Block */}
-            <div >
+            <div>
               <AnimatePresence initial={false} custom={direction} mode="wait">
                 <motion.div
                   key={currentSlide}
@@ -161,15 +165,30 @@ const Hero = () => {
                   }}
                   className="w-full flex flex-col items-center"
                 >
-                  {/* Logo Container */}
+                  {/* Logo Container com imagem responsiva */}
                   <motion.div
-                    className="h-[250px] md:h-[350px] lg:h-[450px] flex items-center justify-center"
+                    className="h-[450px] md:h-[350px] lg:h-[450px] flex items-center justify-center"
                     variants={fadeInUp}
                   >
+                    {/* Imagem para desktop */}
                     <img
                       src={slideContent[currentSlide].logo}
                       alt="Event Logo"
-                      className={`mt-[90px] h-full w-[380px] sm:w-[650px] md:w-[700px] lg:w-[800px] xl:w-[1000px] object-cover transition-all duration-700 ease-out ${currentSlide === 0 ? '' : 'h-[180px] md:h-[300px] lg:h-[440px]'}`}
+                      className={`hidden sm:block mt-[90px] h-full w-[370px] sm:w-[650px] md:w-[700px] lg:w-[800px] xl:w-[1000px] object-cover transition-all duration-700 ease-out ${
+                        currentSlide === 0
+                          ? ""
+                          : "h-[179px] sm:h-[100px] md:h-[300px] lg:h-[440px]"
+                      }`}
+                      loading="eager"
+                    />
+
+                    {/* Imagem para mobile */}
+                    <img
+                      src={slideContent[currentSlide].mobileImage} // Nova propriedade para imagem mobile
+                      alt="Event Logo Mobile"
+                      className={`block sm:hidden mt-[90px] h-full w-full object-cover transition-all duration-700 ease-out ${
+                        currentSlide === 0 ? "" : "h-[179px]"
+                      }`}
                       loading="eager"
                     />
                   </motion.div>
@@ -178,23 +197,21 @@ const Hero = () => {
                   <div className="flex flex-col items-center mt-[2.5rem] w-full space-y-15">
                     {/* Title */}
                     <motion.div variants={fadeInUp}>
-                      <div className="mt-10 text-center">
-               
-                          <span className="h2 xl:h1 font-bold block">
-                            {slideContent[currentSlide].welcome}
-                          </span>
-                          <span className="h2 xl:h1 font-bold inline-block relative">
-                            {slideContent[currentSlide].title}
-                            <img
-                              src={curve}
-                              className="absolute top-full left-0 w-full animate-pulse"
-                              width={624}
-                              height={28}
-                              alt="Curve"
-                              loading="eager"
-                            />
-                          </span>
-                      
+                      <div className="md:mt-10 text-center">
+                        <span className="h2 xl:h1 font-bold block">
+                          {slideContent[currentSlide].welcome}
+                        </span>
+                        <span className="h2 xl:h1 font-bold inline-block relative">
+                          {slideContent[currentSlide].title}
+                          <img
+                            src={curve}
+                            className="absolute top-full left-0 w-full animate-pulse"
+                            width={624}
+                            height={28}
+                            alt="Curve"
+                            loading="eager"
+                          />
+                        </span>
                       </div>
                     </motion.div>
 
@@ -246,7 +263,7 @@ const Hero = () => {
             transition={{ duration: 0.8, delay: 0.8 }}
             className="relative z-10 w-full"
           >
-            <div >
+            <div>
               <CompanyLogos showButton={false} className="hidden lg:block" />
             </div>
           </motion.div>
